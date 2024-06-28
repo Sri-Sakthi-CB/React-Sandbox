2f8f22ca5b2a628855016c55664096617ba46f44
Sri-Sakthi-CB <srisakthi1509@gmail.com>
2024-06-28 09:49:09 +0530

    Update: useEffect and function with async, await

---
+++ useEffect & async await/src/App.js
@@ -0,0 +1,27 @@
+import {useEffect, useState} from 'react';
+
+function App(){
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      async function getData(){
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const data = await response.json();
+        if(data && data.length) setData(data);
+      }
+
+      getData();
+      
+    },[]);
+
+    return(
+      <>
+      <ul>
+        {data.map((item) => (
+          <li key={Math.random()}>{item.title}</li>
+        ))}
+      </ul>
+      </>
+    )
+}
+export default App;
